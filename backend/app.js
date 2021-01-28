3b765eea92c8a5d528fc5f385bf4c49ec417e637
Muhammad Bilal Hanif <bilalhanifhalai@gmail.com>
2021-01-28 21:18:09 +0500

    frontend and backend with graphql

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const {graphqlHTTP} = require('express-graphql');
+const movieSchema = require('./schema/schema');
+const resolvers = require('./resolver/resolver')
+
+mongoose.connect(('mongodb+srv://admin:neduni@cluster0.zy4eu.mongodb.net/moviemaker?retryWrites=true&w=majority'),{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
+.then(()=> console.log("Mongodb connected"))
+.catch((err) => console.log("Error",err));
+
+
+//Setting graphql
+app.use('/graphql',graphqlHTTP({
+    schema:movieSchema,
+    graphiql:true,
+    rootValue:resolvers
+}))
+
+app.get('/',(req,res)=>{
+    res.send("backend running")
+})
+
+app.listen(4000,()=>{
+    console.log("server on port 4000")
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/header'
+import Footer from './components/footer'
+// import Movies from './containers/Movies'
+// import AddMovie from './components/AddMovie'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      {/* <Movies/>
+      <AddMovie/> */}
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
